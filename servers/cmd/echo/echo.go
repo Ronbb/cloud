ca4aba56db64a959aea97eb73f43cafe6e9e79b5
ronbb <ronbiaobiao@gmail.com>
2022-03-15 10:47:34 +0800

    feat: test server

--- servers/cmd/echo/echo.go
+++ servers/cmd/echo/echo.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	consul "github.com/hashicorp/consul/api"
+	internal_service "github.com/ronbb/servers/internal/service"
+	"github.com/ronbb/servers/models"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
@@ -19,5 +24,21 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%v %v", service, meta)
+	fmt.Printf("%v %v\n", service, meta)
+	grpcAddress, ok := service.Meta[internal_service.GRPCAddressLabel]
+	if !ok {
+		panic("grpc address not found")
+	}
+
+	conn, err := grpc.Dial(grpcAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+
+	authentication := models.NewAuthenticationClient(conn)
+	response, err := authentication.Login(context.Background(), &models.LoginRequest{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v\n", response)
 }
