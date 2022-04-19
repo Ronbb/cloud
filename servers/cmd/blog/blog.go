f284879386a363aa49f0f9b66e7aa53d34cf63c7
ronbb <ronbiaobiao@gmail.com>
2022-04-19 09:48:36 +0800

    feat: blog server

--- servers/cmd/blog/blog.go
+++ servers/cmd/blog/blog.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"context"
+
+	"github.com/ronbb/cloud/servers/models"
+)
+
+type BlogServer struct {
+	models.UnimplementedBlogServer
+}
+
+func NewBlogServer() models.BlogServer {
+	return &BlogServer{}
+}
+
+func (bs *BlogServer) CreateArticle(context context.Context, request *models.CreateArticleRequest) (*models.CreateArticleResponse, error) {
+	return nil, nil
+}
